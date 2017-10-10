41b63b1d801d8a87f223bbee312d9a76b9c1fa1e
Luke Couzens <lcouzens@redhat.com>
2017-10-10 19:50:53 +0100

    start of embedded ansible update test

--- cfme/tests/cli/test_appliance_update.py
+++ cfme/tests/cli/test_appliance_update.py
@@ -1,15 +1,27 @@
+import fauxfactory
 import tempfile
 import pytest
 
+from cfme.ansible.repositories import RepositoryCollection
+from cfme.configure.configuration.region_settings import RedHatUpdates
 from cfme.test_framework.sprout.client import SproutClient, SproutException
+from cfme.utils.appliance.implementations.ui import navigate_to
+from cfme.utils.version import current_version
+from cfme.utils.conf import cfme_data
+from cfme.utils.log import logger
+from cfme.utils.update import update
+from cfme.utils.version import Version
+from cfme.utils.wait import wait_for
+from cfme.utils import os, conf
 from fixtures.pytest_store import store
 from scripts.repo_gen import process_url, build_file
-from cfme.utils.version import Version
-from cfme.utils.log import logger
-from cfme.utils.conf import cfme_data
-from cfme.utils import os
 
 versions = []
+
+REPOSITORIES = [
+    "https://github.com/quarckster/ansible_playbooks",
+    "https://github.com/patchkez/ansible_playbooks"
+]
 
 
 def pytest_generate_tests(metafunc):
@@ -27,6 +39,20 @@ def pytest_generate_tests(metafunc):
 
     for i in range(int(minor_build) - 1, -1, -1):
         versions.append("{}.{}.{}".format(split_ver[0], split_ver[1], i))
+
+
+@pytest.yield_fixture(scope='module')
+def ansible_repository(appliance):
+    repositories = RepositoryCollection(appliance=appliance)
+    repository = repositories.create(
+        fauxfactory.gen_alpha(),
+        REPOSITORIES[0],
+        description=fauxfactory.gen_alpha())
+
+    yield repository
+
+    if repository.exists:
+        repository.delete()
 
 
 @pytest.yield_fixture(scope="function")
@@ -52,7 +78,7 @@ def appliance_preupdate(old_version, appliance):
         logger.exception("Couldn't provision appliance with following error:{}".format(e))
         raise SproutException('No provision available')
 
-    apps[0].db.extend_partition()
+    urls = process_url(conf.cfme_data['basic_info'][update_url])
     urls = process_url(cfme_data['basic_info'][update_url])
     output = build_file(urls)
     with tempfile.NamedTemporaryFile('w') as f:
@@ -75,6 +101,56 @@ def test_update_yum(appliance_preupdate, appliance):
     appliance_preupdate.evmserverd.stop()
     with appliance_preupdate.ssh_client as ssh:
         rc, out = ssh.run_command('yum update -y', timeout=3600)
-        assert rc == 0, "update failed {}".format(out)
-    appliance_preupdate.evmserverd.start()
-    assert appliance.version == appliance_preupdate.version
+
+
+@pytest.fixture(scope='module')
+def enabled_embedded_appliance(appliance_preupdate):
+    """Takes a preconfigured appliance and enables the embedded ansible role"""
+    appliance_preupdate.enable_embedded_ansible_role()
+    assert appliance_preupdate.is_embedded_ansible_running
+    return appliance_preupdate
+
+
+@pytest.mark.parametrize('old_version', versions)
+@pytest.mark.uncollectif(lambda: not store.current_appliance.is_downstream)
+@pytest.mark.uncollectif(lambda: current_version() < "5.8")
+def test_embedded_ansible_update(enabled_embedded_appliance, appliance):
+    """ Tests updating an appliance which has embedded ansible role enabled, also confirms that the
+        role continues to function correctly after the update has completed"""
+    set_default_repo = True
+    with enabled_embedded_appliance:
+        red_hat_updates = RedHatUpdates(
+            service='rhsm',
+            url=conf.cfme_data['redhat_updates']['registration']['rhsm']['url'],
+            username=conf.credentials['rhsm']['username'],
+            password=conf.credentials['rhsm']['password'],
+            set_default_repository=set_default_repo
+        )
+        red_hat_updates.update_registration(validate=False)
+        red_hat_updates.check_updates()
+        wait_for(
+            func=red_hat_updates.checked_updates,
+            func_args=[appliance.server.name],
+            delay=10,
+            num_sec=100,
+            fail_func=red_hat_updates.refresh
+        )
+        if red_hat_updates.platform_updates_available():
+            red_hat_updates.update_appliances()
+
+    def is_appliance_updated(appliance):
+        """Checks if cfme-appliance has updated"""
+        assert appliance.version == enabled_embedded_appliance.version
+
+    wait_for(is_appliance_updated, func_args=[enabled_embedded_appliance], num_sec=900)
+    assert wait_for(func=lambda: enabled_embedded_appliance.is_embedded_ansible_running, num_sec=30)
+    assert wait_for(func=lambda: enabled_embedded_appliance.is_rabbitmq_running, num_sec=30)
+    assert wait_for(func=lambda: enabled_embedded_appliance.is_nginx_running, num_sec=30)
+    assert enabled_embedded_appliance.ssh_client.run_command(
+        'curl -kL https://localhost/ansibleapi | grep "Ansible Tower REST API"')
+    updated_description = "edited_{}".format(fauxfactory.gen_alpha())
+    with update(ansible_repository):
+        ansible_repository.description = updated_description
+    view = navigate_to(ansible_repository, "Edit")
+    wait_for(lambda: view.description.value != "", delay=1, timeout=5)
+    assert view.description.value == updated_description
